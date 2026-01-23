21a54565a535f7da084fae4b7f5f0d549be2e8af
Riwaj Chipalu <69961549+Reewaz-C@users.noreply.github.com>
2026-01-23 20:18:18 +0545

    Update outputs.tf

--- terraform/modules/vpc/vpc_sub_modules/subnet/outputs.tf
+++ terraform/modules/vpc/vpc_sub_modules/subnet/outputs.tf
@@ -1,11 +1,9 @@
-output "subnets_id" {
-    description = "Subnet id"
-    value = aws_subnet.subnet[*].id
+output "subnet_ids" {
+  description = "List of subnet IDs created by this module"
+  value       = aws_subnet.subnet[*].id
 }
 
 output "aws_route_table_id" {
     description = "Route table id"
     value = aws_route_table.route_table[*].id
 }
-
-
