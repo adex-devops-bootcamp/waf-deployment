fd3d95a142d9f1df0222199ded37e654ef9ea915
Riwaj Chipalu <69961549+Reewaz-C@users.noreply.github.com>
2026-01-26 08:50:56 +0545

    Update backend.tf

--- terraform/envs/sonarqube/backend.tf
+++ terraform/envs/sonarqube/backend.tf
@@ -1,7 +1,8 @@
 terraform {
     backend "s3" {
-        bucket = "adex-terraform-states-248628534734u56"
+        bucket = "adex-terraform-248628534734u568934547435"
         key = "sonarqube/terraform.tfstate"
         region = "eu-north-1"
     }
+
 }
