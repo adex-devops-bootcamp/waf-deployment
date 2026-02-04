87edf85d400df8580400dbe8b23e372de034cf6f
amit <amit101giri@gmail.com>
2026-02-04 10:14:04 +0545

    UPDATE: ssh for prometheus server

--- terraform/envs/prometheus/main.tf
+++ terraform/envs/prometheus/main.tf
@@ -17,6 +17,12 @@ module "sg" {
       to_port     = 443
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
   ]
 
